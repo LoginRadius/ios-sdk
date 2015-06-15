501b4b6d0b1ca0f0d10efa5de0440ade50076adf
Leyuan <leyuan.ly@outlook.com>
2015-06-15 10:32:20 -0600

    Update documentation for config file

--- Demo/BasicDemo-SocialLogin/BasicDemo2.0/LoginRadiusConfiguration.h
+++ Demo/BasicDemo-SocialLogin/BasicDemo2.0/LoginRadiusConfiguration.h
@@ -1,21 +1,12 @@
-//
-//  LoginRadiusConfiguration.h
-//  BasicDemo2.0
-//
-//  Created by Lucius Yu on 2014-12-05.
-//  Copyright (c) 2014 LoginRadius. All rights reserved.
-//
+/*!
+ @file This file contains the CONST settings
+ @param CLIENT_SITENAME http://support.loginradius.com/hc/en-us/articles/204614109-How-do-I-get-my-LoginRadius-Site-Name-
+ */
 
-#define CLIENT_SITENAME @"lrraaslucius"
-#define API_KEY @"27934297-bbd5-4951-89fb-af620e95adc9"
-#define API_SECRET @"cfab565d-fda1-4ca2-ba7c-26d06d90140e"
+#define CLIENT_SITENAME @"<LoginRadius-Site-Name>"
+#define API_KEY @"<LoginRadius-API-Key>"
+#define API_SECRET @"<LoginRadius-API-Secret>"
 
-//#define CLIENT_SITENAME @"viewpal"
-//#define API_KEY @"6d974a33-2305-4d12-861d-c4f2b072b760"
-//#define API_SECRET @"4aca5ae4-ee83-4f48-af3f-5baee17808a4"
-
-
-#define EMAIL_VERIFICATION @"http://lucius.login4website.com/RaaS-popup-theme/"
-#define FORGOTPASSWORD @"http://lucius.login4website.com/RaaS-popup-theme/"
-
-#define TW_SECRET @"zWmzvrSDDva7Ip0A1l9tNFDk6FwRzhWcFyBnrc5MzHAMDNmK33"
+#define EMAIL_VERIFICATION @"<LoginRadius-Email-Verification-Url>"
+#define FORGOTPASSWORD @"<LoginRadius-Forgot-Password-Url>"
+#define TW_SECRET @"<Twitter-App-Secret>"
