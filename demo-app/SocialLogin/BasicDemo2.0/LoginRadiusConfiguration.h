add31799bec50394dfd5086ac8a90d800327a180
Leyuan <leyuan.ly@outlook.com>
2015-06-16 11:28:27 -0600

    add app

---
+++ demo-app/SocialLogin/BasicDemo2.0/LoginRadiusConfiguration.h
@@ -0,0 +1,9 @@
+/*!
+ @file This file contains the CONST settings
+ @param CLIENT_SITENAME http://support.loginradius.com/hc/en-us/articles/204614109-How-do-I-get-my-LoginRadius-Site-Name-
+ */
+
+#define CLIENT_SITENAME @"<LoginRadius-Site-Name>"
+#define API_KEY @"<LoginRadius-API-Key>"
+#define API_SECRET @"<LoginRadius-API-Secret>"
+#define TW_SECRET @"<Twitter-App-Secret>"
