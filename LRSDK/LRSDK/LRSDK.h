acd31525e556e205047d9d22b97dc5bc058e23ab
Raviteja <ravi@loginradius.com>
2016-05-06 14:51:03 +0530

    removed unnecessary import

--- LRSDK/LRSDK/LRSDK.h
+++ LRSDK/LRSDK/LRSDK.h
@@ -12,14 +12,6 @@ FOUNDATION_EXPORT double LRSDKVersionNumber;
 //! Project version string for LRSDK.
 FOUNDATION_EXPORT const unsigned char LRSDKVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like
-
-
-#if __has_include(<Social/Social.h>) && __has_include(<Accounts/Accounts.h>)
-#import <Social/Social.h>
-#import <Accounts/Accounts.h>
-#endif
-
 #import <LRSDK/LoginRadiusSDK.h>
 
 
