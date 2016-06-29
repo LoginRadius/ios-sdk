3b4d09f289135eda47df3e25c25fd2991f6af5b6
Raviteja Ghanta <ravi@loginradius.com>
2016-06-29 13:27:18 +0530

    Handling network issues
    Checking for network reachability while loading
    hosted pages or social login endpoints, if not
    reachable presenting retry button.
    
    As a result, the webview won't get stuck.

--- sdk/src/LRSDK/LRSDK/LoginRadiusWebLoginViewController.m
+++ sdk/src/LRSDK/LRSDK/LoginRadiusWebLoginViewController.m
@@ -9,12 +9,18 @@
 #import "LoginRadiusUtilities.h"
 #import "NSDictionary+LRDictionary.h"
 #import "LRErrors.h"
+#import "ReachabilityCheck.h"
 
-@interface LoginRadiusWebLoginViewController () <UIWebViewDelegate> {
-	UIWebView * _webView;
-	NSString * _provider;
-}
+@interface LoginRadiusWebLoginViewController () <UIWebViewDelegate>
+
+@property(nonatomic, copy) NSString* provider;
+@property(nonatomic, copy) NSURL * serviceURL;
 @property(nonatomic, copy) LRServiceCompletionHandler handler;
+
+@property(nonatomic, weak) UIWebView* webView;
+@property(nonatomic, weak) UIView *retryView;
+@property(nonatomic, weak) UILabel *retryLabel;
+@property(nonatomic, weak) UIButton *retryButton;
 @end
 
 @implementation LoginRadiusWebLoginViewController
@@ -30,10 +36,47 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	_webView = [[UIWebView alloc] initWithFrame:self.view.frame];
-	_webView.scalesPageToFit = YES;
-	_webView.delegate = self;
-	[self.view addSubview:_webView];
+    UIWebView* webView = [[UIWebView alloc] initWithFrame:self.view.frame];
+    webView.scalesPageToFit = YES;
+    webView.delegate = self;
+    
+    UIView * retryView = [[UIView alloc] initWithFrame:self.view.frame];
+    retryView.backgroundColor = [UIColor whiteColor];
+    retryView.hidden = YES;
+    
+    [self.view addSubview:webView];
+    [self.view addSubview:retryView];
+    [self.view bringSubviewToFront:retryView];
+    
+    UILabel * retryLabel = [[UILabel alloc] initWithFrame:CGRectZero];
+    retryLabel.textColor = [UIColor grayColor];
+    retryLabel.text = @"Please check your network connection and try again.";
+    retryLabel.numberOfLines = 0;
+    retryLabel.textAlignment = NSTextAlignmentCenter;
+    retryLabel.lineBreakMode = NSLineBreakByWordWrapping;
+    retryLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    [retryView addSubview:retryLabel];
+    
+    UIButton *retryButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [retryButton setTitle:@"Retry" forState:UIControlStateNormal];
+    [retryButton sizeToFit];
+    retryButton.translatesAutoresizingMaskIntoConstraints = NO;
+    [retryButton addTarget:self action:@selector(retry:) forControlEvents:UIControlEventTouchUpInside];
+    [retryView addSubview:retryButton];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:retryButton attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:retryView attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0.0f]];
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:retryButton attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:retryView attribute:NSLayoutAttributeCenterY multiplier:1.0f constant:0.0f]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:retryLabel attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:retryView attribute:NSLayoutAttributeCenterX multiplier:1.0f constant:0.0f]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:retryLabel attribute:NSLayoutAttributeWidth relatedBy:NSLayoutRelationEqual toItem:retryView attribute:NSLayoutAttributeWidth multiplier:0.7f constant:0.0f]];
+    
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:retryLabel attribute:NSLayoutAttributeBottom relatedBy:NSLayoutRelationEqual toItem:retryButton attribute:NSLayoutAttributeTop multiplier:1.0f constant:-50.0f]];
+    
+    self.webView = webView;
+    self.retryView = retryView;
+    self.retryLabel = retryLabel;
+    self.retryButton = retryButton;
 }
 
 - (void)viewWillAppear:(BOOL)animated {
@@ -44,11 +87,32 @@
 	self.navigationItem.leftBarButtonItem = cancelItem;
 
 	NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"https://%@.hub.loginradius.com/RequestHandlor.aspx?apikey=%@&provider=%@&ismobile=1", [LoginRadiusSDK siteName], [LoginRadiusSDK apiKey], _provider]];
-	[_webView loadRequest:[NSURLRequest requestWithURL: url]];
+    
+    self.serviceURL = url;
+    [_webView loadRequest:[NSURLRequest requestWithURL: url]];
 }
 
 - (void) cancelPressed {
 	[self finishSocialLogin:NO withError:[LRErrors socialLoginCancelled:_provider]];
+}
+
+- (void)startMonitoringNetwork {
+    ReachabilityCheck* reach = [ReachabilityCheck reachabilityWithHostname:@"cdn.loginradius.com"];
+    reach.unreachableBlock = ^(ReachabilityCheck*reach) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.retryLabel.text = @"Please check your network connection and try again.";
+            self.retryView.hidden = NO;
+        });
+    };
+    
+    [reach startNotifier];
+}
+
+- (void) retry: (id) sender {
+    [self.webView stopLoading];
+    [[NSURLCache sharedURLCache] removeAllCachedResponses];
+    [self.webView loadRequest:[NSURLRequest requestWithURL:self.serviceURL cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:60]];
+    self.retryView.hidden = YES;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -57,7 +121,8 @@
 }
 
 - (void)viewDidLayoutSubviews {
-	_webView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    self.webView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    self.retryView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
 }
 
 #pragma mark - Web View Delegates
@@ -76,6 +141,17 @@
 		return YES;
 	}
 	return YES;
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+    if (error.code == NSURLErrorTimedOut || error.code == NSURLErrorCannotConnectToHost || error.code == NSURLErrorNotConnectedToInternet) {
+        [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"about:blank"]]];
+        self.retryLabel.text = @"Please check your network connection and try again.";
+    } else if (error.code == 101) {
+        self.retryLabel.text = @"Error loading URL, check your API Key & Sitename and try again";
+    }
+    
+    self.retryView.hidden = NO;
 }
 
 - (void)finishSocialLogin:(BOOL)success withError:(NSError*) error {
