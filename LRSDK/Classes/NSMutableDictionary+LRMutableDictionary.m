c7d4eb6630414ddcccbe65348fcfbf21e3f4e67f
Raviteja Ghanta <ravi@loginradius.com>
2016-12-20 01:08:59 +0530

    Fixes Json parsing issue with array of null values
    * Some userfields are returned as array of null values instead
      of empty array

--- LRSDK/Classes/NSMutableDictionary+LRMutableDictionary.m
+++ LRSDK/Classes/NSMutableDictionary+LRMutableDictionary.m
@@ -10,33 +10,41 @@
 
 - (NSMutableDictionary *) replaceNullWithBlank {
 	const NSMutableDictionary *replaced = [NSMutableDictionary dictionaryWithDictionary: self];
-	const id nul = [NSNull null];
-	const NSString *blank = @"";
+	const id nullValue = [NSNull null];
+	const NSString *emptyString = @"";
 
 	for (NSString *key in self) {
-      
 		const id object = [self objectForKey: key];
 
-		if (object == nul) {
-			[replaced setObject: blank forKey: key];
+		if (object == nullValue) {
+			[replaced setObject: emptyString forKey: key];
 		} else if ([object isKindOfClass: [NSDictionary class]]) {
 			NSInteger count = [object count];
-			if ( count != 0 ) {
+
+            if ( count != 0 ) {
 				[replaced setObject: [[object mutableCopy] replaceNullWithBlank] forKey: key];
 			}
 		} else if ( [object isKindOfClass: [NSArray class]] ) {
-			NSMutableArray *tempArray = [NSMutableArray arrayWithArray:[self objectForKey:key]];
-			for (int i=0; i < [tempArray count]; i++) {
-				id arrayItem = [[tempArray objectAtIndex:i] mutableCopy];
-				if([arrayItem isKindOfClass:[NSMutableDictionary class]]) {
-					[tempArray setObject:[arrayItem replaceNullWithBlank] atIndexedSubscript:i];
-				}
+			NSMutableArray *tempArray = [NSMutableArray arrayWithArray:object];
+            NSMutableArray *newArray = [NSMutableArray array];
+
+            for (int i=0; i < [tempArray count]; i++) {
+				id item = [tempArray objectAtIndex:i];
+                if (item != nullValue) {
+                    id mutableItem = [item mutableCopy];
+                    if([mutableItem isKindOfClass:[NSMutableDictionary class]]) {
+                        [newArray setObject:[mutableItem replaceNullWithBlank] atIndexedSubscript:i];
+                    }
+                } else {
+                    [newArray setObject:emptyString atIndexedSubscript:i];
+                }
 			}
-			[replaced setObject:tempArray forKey:key];
+
+			[replaced setObject:newArray forKey:key];
 		}
 	}
 
-  return [replaced mutableCopy];
+    return [replaced mutableCopy];
 }
 
 @end
